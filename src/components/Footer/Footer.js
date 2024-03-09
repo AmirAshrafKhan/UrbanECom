0293c4e6962f9871b48de377a3b88f2ab5ad14ae
AmirAshrafKhan <128276797+AmirAshrafKhan@users.noreply.github.com>
2024-03-09 16:36:34 +0530

    done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer py-5 bg-dark">
+      <div className="container">
+        <div className="footer-content">
+          {" "}
+          <h4 className="footer-items">Urban E Retail Limited</h4>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
